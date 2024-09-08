1d005b0b27dfbb1610b9f8911c4b460aac76fa89
ShailaGonzalez <shailagonzalezcarrero@gmail.com>
2024-09-08 15:56:04 +0200

    Remove deleted files

--- Desktop/mi proyecto/U-Dagger FRONT!/u-dagger-front/src/services/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'http://localhost:3001', // URL del backend
-    timeout: 10000, // Tiempo de espera para las solicitudes
-    headers: {'Content-Type': 'application/json'}
-});
-
-export default instance;
